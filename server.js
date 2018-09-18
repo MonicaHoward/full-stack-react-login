44c378d906fdb750c7a2934268fcf4485b526d34
Monica Howard <jay.m.howard@gmail.com>
2018-09-17 22:56:46 -0500

    Connected Express to MongDb

--- server.js
+++ server.js
@@ -1,5 +1,13 @@
 const express = require("express");
 const app = express();
+const mongoose = require("mongoose");
+
+const db = require("./config/keys").mongoURI;
+
+mongoose
+  .connect(db)
+  .then(() => console.log("MongoDB connected"))
+  .catch(err => console.log(err));
 
 app.get("/", (req, res) => res.send("Hello, World"));
 
